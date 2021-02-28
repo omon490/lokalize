35de264150c6747224baf6de1d5890b424a1c768
omon490 <omonbekmurodov@gmail.com>
2021-03-01 01:03:51 +0500

    add freatures pages html

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import {Switch, Route} from "react-router-dom"
 import Header from './Components/Header/Header';
 import Product from './Components/Pages/Product/Product';
 import Footer from './Components/Footer/Footer';
+import Features from './Components/Pages/Features/Features';
 
 
 function App() {
@@ -13,6 +14,10 @@ function App() {
       <Switch>
         <Route path="/product" exact>
           <Product></Product>
+        </Route>
+
+        <Route path="/features" exact>
+          <Features></Features>
         </Route>
 
 
