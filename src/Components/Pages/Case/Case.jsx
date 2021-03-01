12d552700c5254ab9af53bf943b6be5397746c58
omon490 <omonbekmurodov@gmail.com>
2021-03-01 22:10:48 +0500

    Add case section and styles

--- src/Components/Pages/Case/Case.jsx
+++ src/Components/Pages/Case/Case.jsx
@@ -1,6 +1,9 @@
 import "./Case.css"
 import manImg from "../../../img/man-img.png"
-
+import cake from "../../../img/cake-img.png"
+import userLogo from "../../../img/users-logo.svg"
+import capterra from "../../../img/capterra.svg"
+import star from "../../../img/star.svg"
 function Case() {
   return (
     <div>
@@ -14,7 +17,33 @@ function Case() {
             </div>
 
             <div className="case-right">
-              <img className="case-img" src={manImg} alt=""/>
+              <img className="case-img" src={manImg} alt="man img"/>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+      <div className="translation">
+        <div className="container">
+          <div className="translation-wrapper">
+            <div className="translation-left">
+              <img className="translation-img" src={cake} alt="cake img"/>
+              <p className="translation-left-text">The most-loved translation management software</p>
+            </div>
+
+            <div className="translation-right">
+              <div className="translation-right-left">
+                <img className="translation-right-left-img" src={userLogo} alt="img"/>
+                <img className="translation-right-left-star" src={star} alt="star"/>
+                <p className="translation-right-left-text">Users love us</p>
+              </div>
+
+              <div className="translation-right-right">
+                <img className="translation-right-right-img" src={capterra} alt="img"/>
+                <img className="translation-right-right-star" src={star} alt="star"/>
+                <p className="translation-right-right-text">Highest rated</p>
+              </div>
             </div>
           </div>
         </div>
